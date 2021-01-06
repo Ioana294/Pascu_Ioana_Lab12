39d72c70148149d18caee8fa5d435665d6b9223e
Ioana Pașcu <ioanapascu29@gmail.com>
2021-01-06 17:49:41 +0200

    Add project files.

---
+++ Pascu_Ioana_Lab12/Pascu_Ioana_Lab12/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Pascu_Ioana_Lab12.Data;
+
+namespace Pascu_Ioana_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            //InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Pascu_Ioana_Lab12/Pascu_Ioana_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Pascu_Ioana_Lab12.Models;
+
+namespace Pascu_Ioana_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
